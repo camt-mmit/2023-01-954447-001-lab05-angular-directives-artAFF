6a7689d2a50575951c06caf0fd6de96409b82c3e
artaff <art911191@gmail.com>
2023-07-24 17:01:59 +0700

    origin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TryOneComponent } from './try-one/try-one.component';
+import { TryTwoComponent } from './try-two/try-two.component';
+import { TryThreeComponent } from './try-three/try-three.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'try-one', component: TryOneComponent },
+  { path: 'try-two', component: TryTwoComponent },
+  { path: 'try-three', component: TryThreeComponent },
+];
